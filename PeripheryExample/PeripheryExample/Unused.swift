6c624ba7347447abd170fd4377b4b8f0802e67d0
Arnaud Le Bourblanc <arnaud.lebourblanc@gmail.com>
2020-12-14 00:05:16 +0100

    Add Periphery example

---
+++ PeripheryExample/PeripheryExample/Unused.swift
@@ -0,0 +1,22 @@
+//
+//  Unused.swift
+//  PeripheryExample
+//
+//  Created by Arnaud Le Bourblanc on 13/12/2020.
+//
+
+import Foundation
+
+class UnusedClass {
+    var unusedProperty: String?
+
+    func unusedFunction() {}
+}
+
+protocol UnusedProtocol {}
+
+enum UnusedEnum {}
+
+struct UnusedStruct {}
+
+typealias unusedTypealias = String
